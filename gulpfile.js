184c76b94f51077106e4659f0a494fe3679773b2
Marcus Geil <weblrmail@gmail.com>
2017-10-25 11:26:18 +0200

    Added default files

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+var cssnext = require('postcss-cssnext');
+var precss = require('precss');
+var cssnano = require('cssnano');
+var partialImport = require('postcss-partial-import');
+
+gulp.task('css', function () {
+  var processors = [
+    partialImport,
+    cssnext({
+      warnForDuplicates: false
+    }),
+    precss,
+    cssnano
+  ];
+
+  return gulp.src('./css/style.css')
+    .pipe( sourcemaps.init() )
+    .pipe( postcss(processors) )
+    .pipe( sourcemaps.write('.') )
+    .pipe( gulp.dest('./') );
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./css/*.css', './css/**/*.css'], ['css']);
+});
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+<p class="class">Index</p>
+
+<?php get_footer(); ?>
