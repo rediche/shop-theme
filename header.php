d2e0fdaf2159bda659f55794ce4ba1831fdd0d12
Marcus Geil <weblrmail@gmail.com>
2017-11-13 15:00:06 +0100

    Installed bootstrap grid

--- header.php
+++ header.php
@@ -10,36 +10,40 @@
 <body>
 
 <header class="header">
-  <nav class="top-header">
-    <a href="#" class="top-header__link">Top Link 1</a>
-    <a href="#" class="top-header__link">Top Link 2</a>
-    <a href="#" class="top-header__link">Top Link 3</a>
-  </nav>
+  <div class="container">
 
-  <div class="middle-header">
-    <div class="middle-header__logo">
-      <img src="https://placehold.it/100x50" alt="Logo">
+    <div class="row">
+      <nav class="top-header col">
+        <a href="#" class="top-header__link">Top Link 1</a>
+        <a href="#" class="top-header__link">Top Link 2</a>
+        <a href="#" class="top-header__link">Top Link 3</a>
+      </nav>
     </div>
-    <div class="middle-header__search">
-      <input class="input input--raised" placeholder="Søg">
+
+    <div class="row">
+      <div class="middle-header col">
+        <div class="middle-header__logo">
+          <img src="https://placehold.it/100x50" alt="Logo">
+        </div>
+        <div class="middle-header__search">
+          <input class="input input--raised" placeholder="Søg">
+        </div>
+        <div class="middle-header__shopping-cart">
+          <img src="https://placehold.it/24x24" alt="Shopping cart">
+          <span>Indkøbskurv</span>
+        </div>
+      </div>
     </div>
-    <div class="middle-header__shopping-cart">
-      <img src="https://placehold.it/24x24" alt="Shopping cart">
-      <span>Indkøbskurv</span>
-    </div>
+
   </div>
-
-  <!-- <p>Page <span>title</span></p>
-  <br>
-  <nav class="menu main">
-    <a class="menu__item" href="#">Test</a>
-    <a class="menu__item" href="#">Test</a>
-  </nav>
-  <br> -->
 </header>
 
-<button class="button">Test button</button><br>
-<button class="button button--raised">Test button</button><br>
-
-<input class="input" placeholder="Placeholder"><br>
-<input class="input input--raised" placeholder="Placeholder"><br>
+<nav class="mega-menu">
+  <div class="container">
+    <div class="row">
+      <div class="mega-menu__list col">
+        <p>Test</p>
+      </div>
+    </div>
+  </div>
+</nav>
