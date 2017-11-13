9690759c370a1563aeb2dac3b99a26e82f2ad7e0
Marcus Geil <weblrmail@gmail.com>
2017-11-13 11:41:48 +0100

    Started adding grid layout and various header styles

--- header.php
+++ header.php
@@ -10,13 +10,32 @@
 <body>
 
 <header class="header">
-  <p>Page <span>title</span></p>
+  <nav class="top-header">
+    <a href="#" class="top-header__link">Top Link 1</a>
+    <a href="#" class="top-header__link">Top Link 2</a>
+    <a href="#" class="top-header__link">Top Link 3</a>
+  </nav>
+
+  <div class="middle-header">
+    <div class="middle-header__logo">
+      <img src="https://placehold.it/100x50" alt="Logo">
+    </div>
+    <div class="middle-header__search">
+      <input class="input input--raised" placeholder="Søg">
+    </div>
+    <div class="middle-header__shopping-cart">
+      <img src="https://placehold.it/24x24" alt="Shopping cart">
+      <span>Indkøbskurv</span>
+    </div>
+  </div>
+
+  <!-- <p>Page <span>title</span></p>
   <br>
   <nav class="menu main">
     <a class="menu__item" href="#">Test</a>
     <a class="menu__item" href="#">Test</a>
   </nav>
-  <br>
+  <br> -->
 </header>
 
 <button class="button">Test button</button><br>
