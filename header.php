5e5cb9f2e1184621554b716125309a2776377e7c
Marcus Geil <weblrmail@gmail.com>
2017-11-13 21:48:31 +0100

    Started work on responsive header

--- header.php
+++ header.php
@@ -22,28 +22,52 @@
 
     <div class="row">
       <div class="middle-header col">
+        <button class="middle-header__menu-toggle">
+          <svg  class="middle-header__menu-toggle-icon">
+            <g id="menu"><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></g>
+          </svg>
+        </button>
+
         <div class="middle-header__logo">
-          <img src="https://placehold.it/100x50" alt="Logo">
+          <!-- <img src="https://placehold.it/100x50" alt="Logo"> -->
         </div>
+
         <div class="middle-header__search">
-          <input class="input input--raised" placeholder="Søg">
+          <button class="middle-header__search-toggle">
+            <svg class="middle-header__search-icon">
+              <g id="search"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></g>
+            </svg>
+          </button>
+          <input class="input input--raised middle-header__search-input" placeholder="Søg">
         </div>
-        <div class="middle-header__shopping-cart">
-          <img src="https://placehold.it/24x24" alt="Shopping cart">
-          <span>Indkøbskurv</span>
-        </div>
+
+        <a class="middle-header__shopping-cart" href="#">
+          <svg class="middle-header__shopping-cart-icon">
+            <g id="shopping-cart"><path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z"/></g>
+          </svg>
+          <!-- <span>Indkøbskurv</span> -->
+        </a>
       </div>
     </div>
 
   </div>
 </header>
 
-<nav class="mega-menu">
+<nav class="mega-menu mega-menu--open">
   <div class="container">
     <div class="row">
+
       <div class="mega-menu__list col">
-        <p>Test</p>
+        <div class="mega-menu__list-item">
+          <a href="#" class="mega-menu__list-item-link">Hovedkategori</a>
+          <div class="mega-menu__sub-list">
+            <div class="mega-menu__list-item">
+              <a href="#" class="mega-menu__list-item-link">Overkategori</a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+
   </div>
 </nav>
