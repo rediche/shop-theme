c64b190f06ab3f05092ed213d7e6fde689f02db5
Marcus Geil <weblrmail@gmail.com>
2017-11-14 11:56:29 +0100

    Initial front-page boxes

---
+++ front-page.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+<div class="front-page">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <a class="card category-entry" href="#">
+          <div class="category-entry__inner">
+            <h2>Til lands</h2>
+          </div>
+        </a>
+      </div>
+      <div class="col">
+        <a class="card category-entry" href="#">
+          <div class="category-entry__inner">
+            <h2>Til vands</h2>
+          </div>
+        </a>
+      </div>
+      <div class="col">
+        <a class="card category-entry" href="#">
+          <div class="category-entry__inner">
+            <h2>I luften</h2>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
