3e9179183910bd4b31db8d715c338ff226c8e563
Marcus Geil <weblrmail@gmail.com>
2017-11-14 13:13:23 +0100

    Added images to category entries

--- front-page.php
+++ front-page.php
@@ -5,21 +5,21 @@
     <div class="row">
       <div class="col">
         <a class="card category-entry" href="#">
-          <div class="category-entry__inner">
+          <div class="category-entry__inner" style="background-image:url('<?php echo get_template_directory_uri() ?>/images/categories/til-lands-spejlvendt.jpg');">
             <h2>Til lands</h2>
           </div>
         </a>
       </div>
       <div class="col">
         <a class="card category-entry" href="#">
-          <div class="category-entry__inner">
+          <div class="category-entry__inner" style="background-image:url('<?php echo get_template_directory_uri() ?>/images/categories/til-vands.jpg');">
             <h2>Til vands</h2>
           </div>
         </a>
       </div>
       <div class="col">
         <a class="card category-entry" href="#">
-          <div class="category-entry__inner">
+          <div class="category-entry__inner" style="background-image:url('<?php echo get_template_directory_uri() ?>/images/categories/i-luften.jpg');">
             <h2>I luften</h2>
           </div>
         </a>
