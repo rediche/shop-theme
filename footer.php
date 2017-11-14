b3a865c5dc758b6b179fb66b481e7cfbba152d32
Marcus Geil <weblrmail@gmail.com>
2017-11-14 14:41:56 +0100

    Added products to frontpage

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div class="footer">
   <div class="container">
     <div class="row">
-      <div class="col">
+      <div class="col-sm-12 col-md-4 col-lg-6">
         <img class="footer__logo" src="<?php echo get_template_directory_uri() ?>/images/holtehobby-logo.svg" alt="Logo">
         <p class="footer__about">Holte Modelhobby<br>
         Øverødvej 5, 2840 Holte<br>
@@ -9,12 +9,12 @@
         <a href="mailto:info@holte-modelhobby.dk">info@holte-modelhobby.dk</a></p>
       </div>
       <?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
-        <div class="col" role="complementary">
+        <div class="col-sm-12 col-md-4 col-lg-3" role="complementary">
           <?php dynamic_sidebar( 'footer-1' ); ?>
         </div>
       <?php endif; ?>
       <?php if ( is_active_sidebar( 'footer-2' ) ) : ?>
-        <div class="col" role="complementary">
+        <div class="col-sm-12 col-md-4 col-lg-3" role="complementary">
           <?php dynamic_sidebar( 'footer-2' ); ?>
         </div>
       <?php endif; ?>
--- front-page.php
+++ front-page.php
@@ -3,26 +3,74 @@
 <div class="front-page">
   <div class="container">
     <div class="row">
-      <div class="col">
+      <div class="col-sm-12 col-md-4">
         <a class="card category-entry" href="#">
           <div class="category-entry__inner" style="background-image:url('<?php echo get_template_directory_uri() ?>/images/categories/til-lands-spejlvendt.jpg');">
             <h2>Til lands</h2>
           </div>
         </a>
       </div>
-      <div class="col">
+      <div class="col-sm-12 col-md-4">
         <a class="card category-entry" href="#">
           <div class="category-entry__inner" style="background-image:url('<?php echo get_template_directory_uri() ?>/images/categories/til-vands.jpg');">
             <h2>Til vands</h2>
           </div>
         </a>
       </div>
-      <div class="col">
+      <div class="col-sm-12 col-md-4">
         <a class="card category-entry" href="#">
           <div class="category-entry__inner" style="background-image:url('<?php echo get_template_directory_uri() ?>/images/categories/i-luften.jpg');">
             <h2>I luften</h2>
           </div>
         </a>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="product-section__header">
+          <h2 class="product-section__title">Nuværende tilbud</h2>
+          <a href="#" class="product-section__see-more">Se flere</a>
+        </div>
+      </div>
+
+      <div class="col-6 col-md-4 col-lg-3">
+        <div class="product-card card">
+          <div class="product-card__image"></div>
+          <div class="product-card__info">
+            <h3 class="product-card__title">Product title</h3>
+            <p class="product-card__price">999,95 kr.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-6 col-md-4 col-lg-3">
+        <div class="product-card card">
+          <div class="product-card__image"></div>
+          <div class="product-card__info">
+            <h3 class="product-card__title">Product titleProduct titleProduct titleProduct title</h3>
+            <p class="product-card__price">999,95 kr.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-6 col-md-4 col-lg-3">
+        <div class="product-card card">
+          <div class="product-card__image"></div>
+          <div class="product-card__info">
+            <h3 class="product-card__title">Product title</h3>
+            <p class="product-card__price">999,95 kr.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-6 col-md-4 col-lg-3">
+        <div class="product-card card">
+          <div class="product-card__image"></div>
+          <div class="product-card__info">
+            <h3 class="product-card__title">Product title</h3>
+            <p class="product-card__price">999,95 kr.</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
