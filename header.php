da5cc9da0c488fb9117f3fba1cee182843e0e45d
Marcus Geil <weblrmail@gmail.com>
2017-11-14 10:43:43 +0100

    Added logo to mobile mega-menu

--- header.php
+++ header.php
@@ -55,6 +55,11 @@
 
 <nav class="mega-menu">
   <div class="container">
+    <div class="row">
+      <div class="col">
+        <img class="mega-menu__logo" src="<?php echo get_template_directory_uri() ?>/images/holtehobby-logo-dark.svg" alt="Logo">
+      </div>
+    </div>
     <?php 
     wp_nav_menu( array(
       'theme-location' => 'mega-menu',
