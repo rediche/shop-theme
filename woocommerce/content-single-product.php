5e93b0a739013eda8dc8fe1db56349ab3c8c8bbc
Marcus Geil <weblrmail@gmail.com>
2017-11-20 09:25:10 +0100

    Restyled related products on product page

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -104,8 +104,19 @@ if ( ! defined( 'ABSPATH' ) ) {
          * @hooked woocommerce_upsell_display - 15
          * @hooked woocommerce_output_related_products - 20
          */
-        do_action( 'woocommerce_after_single_product_summary' );
+        woocommerce_output_product_data_tabs();
+        //do_action( 'woocommerce_after_single_product_summary' );
       ?>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">  
+        <?php woocommerce_upsell_display(); ?>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">  
+        <?php woocommerce_output_related_products(); ?>
       </div>
     </div>
   </div>
