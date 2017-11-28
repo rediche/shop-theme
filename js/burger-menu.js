aa9000a25c6e74d3f418a68e04cb43c26212b1c4
Marcus Geil <weblrmail@gmail.com>
2017-11-28 10:03:30 +0100

    Add menu-item toggles amongst more

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,6 +1,9 @@
 // Don't do stuff until DOMContentLoaded happens.
 document.addEventListener("DOMContentLoaded", function(event) { 
-  enableBurgerMenuToggle();
+  const mediaQuery = window.matchMedia('(max-width: 768px)');
+
+  // Only run stuff, if we're on mobile devices
+  if (mediaQuery.matches) enableBurgerMenuToggle();
 });
 
 function enableBurgerMenuToggle() {
@@ -16,5 +19,22 @@ function enableBurgerMenuToggle() {
 
   burgerMenuClose.addEventListener('click', function() {
     burgerMenu.classList.remove('mega-menu--open');
+  });
+
+  enableMenuToggles();
+}
+
+// Get all menu items with children on the top level
+function enableMenuToggles() {
+  const menuItemsWithChildren = document.querySelectorAll('.mega-menu__list > .menu-item-has-children');
+
+  menuItemsWithChildren.forEach(function(menuItemWithChild) {
+    const menuItemWithChildLink = menuItemWithChild.querySelector('a');
+
+    if (menuItemWithChildLink) menuItemWithChildLink.removeAttribute('href');
+
+    menuItemWithChild.addEventListener('click', function() {
+      menuItemWithChild.classList.toggle('menu-item--open');
+    });
   });
 }
