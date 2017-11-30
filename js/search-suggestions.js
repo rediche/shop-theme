6f14409a84275e596c89abf00594f275a6b6593c
Marcus Geil <weblrmail@gmail.com>
2017-11-30 22:36:07 +0100

    Added see more results

--- js/search-suggestions.js
+++ js/search-suggestions.js
@@ -3,20 +3,54 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function initSearchSuggestions() {
-  console.log("Search suggestions running...");
-  console.log("Base URL", BASE_URL);
+  const SEARCH_FORM = document.querySelector('.search__form');
+  const SEARCH_INPUT = document.querySelector('[data-search-input]');
+  const SEARCH_RESULTS = document.querySelector('.search__results');
+  const MORE_RESULTS = document.querySelector('.search__more');
+  const TYPE_INTERVAL = 500;
+  let typingTimer;
 
-  runSearch();
+  if (SEARCH_INPUT && SEARCH_RESULTS && MORE_RESULTS) {
+    // Når man har fokus på inputfeltet
+    SEARCH_INPUT.addEventListener('click', function(event) {
+      SEARCH_INPUT.classList.add('search__input--has-results');
+      SEARCH_RESULTS.classList.add('search__results--open');
+    });
+
+    // Når man klikker væk fra inputfeltet
+    document.body.addEventListener('click', function (event) {
+      if (!SEARCH_FORM.contains(event.target)) {
+        SEARCH_INPUT.classList.remove('search__input--has-results');
+        SEARCH_RESULTS.classList.remove('search__results--open');
+      }
+    });
+
+    // Når man giver slip på en key
+    SEARCH_INPUT.addEventListener('keyup', function(event) {
+      clearTimeout(typingTimer);
+      typingTimer = setTimeout(function() {
+          shouldRunSearch(event.target.value);
+      }, TYPE_INTERVAL);
+    });
+
+    MORE_RESULTS.addEventListener('click', function(event) {
+      console.log("click");
+      SEARCH_FORM.submit();
+    });
+  }
 }
 
-function runSearch() {
-  const PRODUCTS_ENDPOINT = '/wp-json/wp/v2/product';
-  const SEARCH_INPUT = document.querySelector('[data-search-input]');
+function shouldRunSearch(value) {
+  clearResults(); // Clear current results
+  if (value) runSearch(value); // Only run search if value is set
+}
 
-  fetch(`${BASE_URL}${PRODUCTS_ENDPOINT}?search=${SEARCH_INPUT.value}&_embed`).then(function(response) {
+function runSearch(value) {
+  const PRODUCTS_ENDPOINT = '/wp-json/wp/v2/product';
+
+  fetch(`${BASE_URL}${PRODUCTS_ENDPOINT}?search=${value}&_embed`).then(function(response) {
     return response.json();
   }).then(function(json) {
-    console.log(json);
     renderProducts(json);
   });
 } 
@@ -25,22 +59,28 @@ function renderProducts (json) {
   if (!json) return;
 
   // Hent template
-  const TEMPLATE = document.querySelector('#search-result');
+  const SEARCH_TEMPLATE = document.querySelector('#search-result');
   const SEARCH_RESULTS = document.querySelector('[data-search-results]');
 
   // Loop gennem produkter
   json.forEach(function(product) {
-    const RESULT_MARKUP = TEMPLATE.content.cloneNode(true);
-    
+    const RESULT_MARKUP = SEARCH_TEMPLATE.content.cloneNode(true);
+
+    // Fyld data ind
     RESULT_MARKUP.querySelector('.search__result').href = product.link; // Product URL
     RESULT_MARKUP.querySelector('.search__result-image').src = product._embedded['wp:featuredmedia'][0].media_details.sizes.thumbnail.source_url; // Image URL
     RESULT_MARKUP.querySelector('.search__result-name').innerHTML = product.title.rendered; // Title
     RESULT_MARKUP.querySelector('.search__result-price').innerHTML = `${product._regular_price} DKK`; // Price
 
-    // Add to results container
+    // Indsæt på resultatlisten
     SEARCH_RESULTS.appendChild(RESULT_MARKUP);
   });
-  // Fyld data ind
-  // Indsæt på resultatlisten
+  
   // Indsæt flere resultater knap
+  
+}
+
+function clearResults() {
+  const SEARCH_RESULTS = document.querySelector('[data-search-results]');
+  SEARCH_RESULTS.innerHTML = '';
 }
--- partials/search-field.php
+++ partials/search-field.php
@@ -5,8 +5,11 @@
         <g id="search"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></g>
       </svg>
     </button>
-    <input class="input search__input search__input--has-results" value="<?php echo get_search_query(); ?>" placeholder="Søg" name="s" data-search-input>
+    <input class="input search__input" value="<?php echo get_search_query(); ?>" placeholder="Søg" name="s" data-search-input autocomplete="off">
     <input type="hidden" name="post_type" value="product" />
-    <div class="search__results list card" data-search-results></div>
+    <div class="search__results list card">
+      <div data-search-results></div>
+      <a class="search__more">Se flere resultater</a>
+    </div>
   </form>
 </div>
