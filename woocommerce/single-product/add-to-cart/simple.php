9f54378ac9cc67955eee1e1cdb6d574e4af82a7e
Mads Emil Hansen <afrohansen@gmail.com>
2017-12-08 10:46:16 +0100

    Add to cart renamed

--- woocommerce/single-product/add-to-cart/simple.php
+++ woocommerce/single-product/add-to-cart/simple.php
@@ -56,7 +56,7 @@ if ( $product->is_in_stock() ) : ?>
       do_action( 'woocommerce_after_add_to_cart_quantity' );
     ?>
 
-    <button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt add-to-cart__button button--raised"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+    <button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt add-to-cart__button button--raised">Tilføj til vogn<?php //echo esc_html( $product->single_add_to_cart_text() ); ?></button>
 
     <?php
       /**
